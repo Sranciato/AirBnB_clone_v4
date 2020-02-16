92a88c2e13743461a705153e900e4ce884cd2153
sranciato <sranciato1@gmail.com>
2020-02-16 23:52:45 +0000

    [ADD] 1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+const idList = [];
+const nameList = [];
+$(() => {
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
+      idList.push($(this).data('id'));
+      nameList.push($(this).data('name'));
+    } else {
+      const index = idList.indexOf($(this).data('id'));
+      idList.splice(index, 1);
+      nameList.splice(index, 1);
+    }
+    if (idList.length < 1) {
+      $('.amenities h4').text('\xa0');
+    } else {
+      $('.amenities h4').text(nameList);
+    }
+  });
+});

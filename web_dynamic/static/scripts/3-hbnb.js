fdb66d2476216284e2bfc4ac4211a6632b3e90af
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-17 16:35:54 -0500

    [ADD] Function to add article for place

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,5 +1,44 @@
 const idList = [];
 const nameList = [];
+
+function htmlForPlace (place){
+  const article = $(`
+    <article>
+      <div class="title">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">
+          ${place.price_by_night}
+        </div>
+      </div>
+      <div class="information">
+        <div class="max_guest">
+          <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+          <br/>
+          ${place.max_guest} Guests
+        </div>
+        <div class="number_rooms">
+          <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+          <br />
+          ${place.number_rooms} Bedrooms
+        </div>
+        <div class="number_bathrooms">
+          <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+          <br />
+          ${place.number_bathrooms} Bathroom
+        </div>
+      </div>
+      <div class="user">
+        <strong>Owner: ${users[place.user_id]}</strong>
+      </div>
+      <div class="description">
+        ${place.description}
+      </div>
+    </article>
+    `);
+    $('.places').append(article)
+}
+
+
 $(() => {
   $('input[type="checkbox"]').click(function () {
     if ($(this).prop('checked') === true) {
@@ -26,7 +65,7 @@ $(() => {
     url: 'http://0.0.0.0:5001/api/v1/places_search',
     data: {},
     contentType: application/json,
-    success: success,
+    success: htmlForPlace(place),
     dataType: dataType
   });
 });
